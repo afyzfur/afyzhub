1e3de36c9f3359795b4c3ae8ce6301fb3a2f4ae3
afyzfur <16864999@qq.com>
2026-07-19 14:55:50 +0800

    add app/src/main/java/com/afyzhub/aichat/MainActivity.kt

---
+++ app/src/main/java/com/afyzhub/aichat/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.afyzhub.aichat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import com.afyzhub.aichat.ui.AfyzhubApp
+import com.afyzhub.aichat.ui.theme.afyzhubTheme
+
+class MainActivity : ComponentActivity() {
+    private val viewModel: ChatViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            afyzhubTheme {
+                AfyzhubApp(viewModel)
+            }
+        }
+    }
+}
