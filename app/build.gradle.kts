6f065ee18da32408ced99380c8e813a3331a9f20
afyzfur <16864999@qq.com>
2026-09-24 00:24:49 +0800

    chore: versionName 加时间戳便于区分测试包

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 29
-        versionName = "0.3.7-dev" // complete with full changelog
+        versionName = "0.3.7-dev-092316"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
